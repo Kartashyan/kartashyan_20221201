f16ce5421024649fb6767a5925e418b7694a5209
Yesh <yeghishe.kartashyan@gmail.com>
2022-12-01 23:09:11 +0100

    add test for create video success case

--- app/models/video.server.test.ts
+++ app/models/video.server.test.ts
@@ -1,6 +1,23 @@
-export {};
+import { Video } from "@prisma/client";
+import { Context, MockContext } from "~/db.context.server";
+import { createVideo } from "./video.server";
+
+let mockCtx: MockContext;
+let ctx: Context;
+
 describe("video model", () => {
-    it.todo("should create a video item");
+    it("should create a video item", async () => {
+        const videoItem: Video = {
+            id: "random id",
+            title: "Brain training",
+            thumbnail: "asdfadfsdf242jwqew",
+            categoryName: "Education",
+        };
+        mockCtx.prisma.video.create.mockResolvedValue(videoItem);
+
+        await expect(createVideo(videoItem, ctx)).resolves.toEqual(videoItem);
+
+    });
     it.todo("should fail if category does not mutch acceptable category list");
     it.todo("should get a video item list");
 });
---
+++ app/models/video.server.ts
@@ -0,0 +1,4 @@
+import { Video } from "@prisma/client";
+import { Context } from "~/db.context.server";
+
+export const createVideo = async (videoItem: Partial<Video>, ctx: Context) => {};
