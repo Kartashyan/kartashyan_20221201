7f3a555997cfb89630ec4b535478f18a470a1420
Yesh <yeghishe.kartashyan@gmail.com>
2022-12-01 22:43:05 +0100

    add test cases for video model

---
+++ app/models/video.server.test.ts
@@ -0,0 +1,6 @@
+export {};
+describe("video model", () => {
+    it.todo("should create a video item");
+    it.todo("should fail if category does not mutch acceptable category list");
+    it.todo("should get a video item list");
+});
